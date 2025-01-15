e20222e6d411ee18c6129942fadf3137a4f5f2f3
Ankit Karnwal <karnwalankit89@gmail.com>
2025-01-15 23:04:09 +0530

    Update otp.js

--- otp.js
+++ otp.js
@@ -1,7 +1,7 @@
 const crypto = require("node:crypto");
 
 function generateOtp() {
-  const otp = crypto.randomInt(1000, 10000);
+  const otp = crypto.randomInt(1000, 10000); //Generate four digits random number. Add 0 to get more digits as your needs
   return otp;
 }
 
